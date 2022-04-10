a5cb47688ef8434f53ea7d5bb7824dbf2439309e
Mauricio Giuliani <mauricio_giuliani@dell.com>
2022-04-10 12:26:31 -0300

    wip

---
+++ src/index.ts
@@ -0,0 +1,84 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+import { Team } from "./model/team";
+import { Match } from './model/match';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+
+const lista = require("./assets/lista.json");
+
+app.get('/', (req: Request, res: Response) => {
+
+  let teams: Team[] = [];
+
+  const listaDireita: string[] = suffle(lista.direita);
+  const listaEsquerda: string[] = suffle(lista.esquerda);
+
+  for (let i = 0; i < listaDireita.length; i++) {
+    const team: Team = {
+      playerA: {
+        name: listaDireita[i]
+      },
+      playerB: {
+        name: listaEsquerda[i]
+      }
+    }
+
+    teams.push(team);
+  }
+
+  console.log(teams)
+
+
+  res.send(suffle(suffle(sortGroupsOneLeg(teams))));
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
+
+
+
+function suffle(array: any[]): any[] {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+function sortGroupsOneLeg(teams: Team[]) : Match[] {
+  let matches: Match[] = [];
+  let currentIndex = 0;
+
+  while (currentIndex < teams.length) {
+    teams.forEach((team: Team, index: number) => {
+      if (currentIndex > index) {
+        matches.push({
+          teamA: team,
+          teamB: teams[currentIndex]
+        })
+      }
+    });
+
+    currentIndex++;
+  }
+
+  return matches;
+}
+
+
+  // return table;
